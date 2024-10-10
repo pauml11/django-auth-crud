edc2dbcec875c01a0a8f40fe2b7c9992426cd335
pauml11 <dannapau.ml01@gmail.com>
2024-10-09 21:24:02 -0500

    Update views.py

--- tasks/views.py
+++ tasks/views.py
@@ -8,8 +8,6 @@ from .forms import TaskForm
 from .models import Task
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
-from django.shortcuts import render, redirect
 
 def home(request):
     return render(request, "home.html")
@@ -111,17 +109,18 @@ def signout(request):
     return redirect('home')
 
 def signin(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-
-        if user is not None:
-            login(request, user)
-            return redirect('home')  # Redirigir a una página de éxito.
+    if request.method == 'GET':
+        return render(request, 'signin.html', 
+                  {'form': AuthenticationForm})
+    else:
+        user = authenticate(request, username=request.POST['username'],
+                      password=request.POST['password'])
+        if user is None:
+              return render(request, 'signin.html', {
+                  'form': AuthenticationForm,
+                  'error':'Username or password is incorrect'
+                  })
         else:
-            messages.error(request, 'Usuario o contraseña inválidos.')  # Agregar un mensaje de error
-            return render(request, 'signin.html', {'error': 'Credenciales inválidas'})
-
-    return render(request, 'signin.html')
+            login(request, user)
+            return redirect('tasks')
     
